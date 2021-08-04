28befebb6ed390e1598bf57851286a04545d0124
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-04 00:21:21 -0300

    Update exemplo.

--- example-json.js
+++ example-json.js
@@ -36,3 +36,36 @@ const example1 =
       }
     ]
   }
+
+const example2 =
+{
+  "data":[
+    {
+      "source": "A", "target": "B", "distance": 5
+    },
+    {
+      "source": "B", "target": "C", "distance": 4
+    },
+    {
+      "source": "C", "target": "D", "distance": 8
+    },
+    {
+      "source": "D", "target": "C", "distance": 8
+    },
+    {
+      "source": "D", "target": "E", "distance": 6
+    },
+    {
+      "source": "A", "target": "D", "distance": 5
+    },
+    {
+      "source": "C", "target": "E", "distance": 2
+    },
+    {
+      "source": "E", "target": "B", "distance": 3
+    },
+    {
+      "source": "A", "target": "E", "distance": 7
+    }
+  ]
+}
