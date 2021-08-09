7ada9321581c54cc0fc990605bf535dc6e65ff37
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-09 18:13:54 -0300

    Removendo rotas de teste.

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from typing import Optional,List
+from graphHandler import Graph, Distance
+from graphHandler import getAllRoutes, getMinDistance
+from typing import Optional, List
 from fastapi import FastAPI
-from deta import Deta
-import os
 from dotenv import load_dotenv
 load_dotenv("./.env")
 
-from graphHandler import getAllRoutes, getMinDistance
-from graphHandler import Graph, Distance
-
-# deta = Deta(os.environ['DETA_KEY'])
-deta = Deta()
-graphsDB = deta.Base("graphs")
 graphsDB: List[Graph] = []
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -22,6 +17,7 @@ def read_root():
         "Idade": 27,
         "Data da Vacina": "04/08/21"
     }
+
 
 @app.post("/graph")
 def create_graph(graph: Graph):
@@ -32,34 +28,24 @@ def create_graph(graph: Graph):
     graphsDB.append(graph)
     return graphsDB[-1]
 
+
 @app.get("/graph/{graphId}")
 def get_graph(graphId: int):
     for graph in graphsDB:
         if(graph.id == graphId):
             return graph
 
+
 @app.post("/routes/{graphId}/from/{town1}/to/{town2}")
 def get_all_routes(graphId: int, town1: str, town2: str, maxStops: int):
     currentGraph = [graph for graph in graphsDB if graph.id == graphId][0]
-    return getAllRoutes(currentGraph,town1,town2,maxStops)
+    return getAllRoutes(currentGraph, town1, town2, maxStops)
 
-# TEST VERSION
-@app.get("/routes/{graphId}/from/{town1}/to/{town2}")
-def get_all_routes(graphId: int, town1: str, town2: str, maxStops: int):
-    currentGraph = [graph for graph in graphsDB if graph.id == graphId][0]
-    return getAllRoutes(currentGraph,town1,town2,maxStops)
 
 @app.post("/distance/{graphId}/from/{town1}/to/{town2}")
 def get_min_distance(graphId: int, town1: str, town2: str):
     currentGraph = [graph for graph in graphsDB if graph.id == graphId][0]
-    (_,minPath,_,minDistance) = getMinDistance(currentGraph,town1,town2)
-    return Distance(distance=minDistance,path=minPath)
-
-# TEST VERSION
-@app.get("/distance/{graphId}/from/{town1}/to/{town2}")
-def get_min_distance(graphId: int, town1: str, town2: str):
-    currentGraph = [graph for graph in graphsDB if graph.id == graphId][0]
-    (_,minPath,_,minDistance) = getMinDistance(currentGraph,town1,town2)
-    return Distance(distance=minDistance,path=minPath)
+    (_, minPath, _, minDistance) = getMinDistance(currentGraph, town1, town2)
+    return Distance(distance=minDistance, path=minPath)
 
 # py -m uvicorn main:app --reload
